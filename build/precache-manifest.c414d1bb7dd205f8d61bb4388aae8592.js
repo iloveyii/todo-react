7b7443077b6719ac2ab5505726ecf8028ec58412
Hazrat Ali <iloveyii@yahoo.com>
2019-05-02 23:08:54 +0200

    Added build in repo

---
+++ build/precache-manifest.c414d1bb7dd205f8d61bb4388aae8592.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e8c9f5d0b882a0228e5f3bee0b13ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2f90f290483dceb44fb",
+    "url": "/static/css/main.28b0a659.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "e2f90f290483dceb44fb",
+    "url": "/static/js/main.160e4b1d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
